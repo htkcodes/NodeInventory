a603f1501ae70ced559e9afa8e8ca6ddf5e0152f
Reneicko Getten <rkgetten@gmail.com>
2017-11-02 02:34:58 -0400

    update & delete function added

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -25,37 +25,110 @@ exports.item_list = function(req, res, next) {
         });
         
     };
-// Display detail page for a specific item
-exports.item_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: item detail: ' + req.params.id);
-};
 
 // Display item create form on GET
 exports.item_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create GET');
+    res.render('item_form', { title: 'item Genre' });
 };
-
 // Handle item create on POST
-exports.item_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create POST');
-};
+exports.item_create_post = function(req, res,next) {
+    req.checkBody('name', 'Item name must be specified.').notEmpty(); //We won't force Alphanumeric, because people might have spaces.
+    req.checkBody('quantit', 'Quantity must not be empty').isEmpty();
+    req.checkBody('price', 'Price must not be empty').notEmpty();
 
-// Display item delete form on GET
-exports.item_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: item delete GET');
-};
+    
+    req.sanitize('name').escape();
+    req.sanitize('quantity').escape();
+    req.sanitize('price').escape();
+    req.sanitize('name').trim();     
+    req.sanitize('quantity').trim();
+    req.sanitize('price').trim();
 
+
+    var errors = req.validationErrors();
+    
+    var items = new item(
+      { name: req.body.name, 
+        quantity: req.body.quantity, 
+        price: req.body.price,
+       });
+       
+    if (errors) {
+        res.render('item_form', { title: 'Item Author', item: items, errors: errors});
+    return;
+    } 
+    else {
+    // Data from form is valid
+    
+        items.save(function (err) {
+            if (err) { return next(err); }
+               //successful - redirect to new author record.
+               res.redirect('/');
+            });
+    }
+
+};
+exports.item_delete_get=function(req,res,next){
+    res.render('item_delete',{title:'Delete Item'});
+};
 // Handle item delete on POST
 exports.item_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: item delete POST');
-};
+    req.checkBody('itemname', 'Item name must exist').notEmpty(); 
 
+    var errors = req.validationErrors();
+    
+    if (errors) {
+        res.render('item_delete', { title: 'Delete Items', errors: errors});
+    return;
+    }
+    else{
+        item.findOneAndRemove(req.body.itemname,function deleteItem(err){
+            if(err){return next(err);}
+            res.redirect('/inventory/items');
+        });
+
+    }
+    
+          
+   
+};
 // Display item update form on GET
 exports.item_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: item update GET');
+    res.render('item_update',{title:'Update Item'});
 };
 
 // Handle item update on POST
-exports.item_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: item update POST');
+exports.item_update_post = function(req, res,next) {
+    req.checkBody('quantit', 'Quantity must not be empty').isEmpty();
+    req.checkBody('price', 'Price must not be empty').notEmpty();
+
+    
+    req.sanitize('quantity').trim();
+    req.sanitize('quantity').escape();
+   
+    req.sanitize('price').escape();    
+  
+    req.sanitize('price').trim();
+
+    var items = new item(
+        {  name: req.body.name, 
+          quantity: req.body.quantity, 
+          price: req.body.price,
+          _id:req.body._id
+         });
+    
+    var errors = req.validationErrors();
+ 
+     
+    if (errors) {
+        res.render('item_update', { title: 'Update Items',item:items, errors: errors});
+    return;
+    }
+    else{
+        item.findOneAndUpdate(req.body.itemname,items,function updateItem(err){
+            if(err){return next(err);}
+            res.redirect('/inventory/items');
+        });
+
+    }
 };
--- routes/inventory.js
+++ routes/inventory.js
@@ -15,20 +15,15 @@ router.get('/item/create', item_controller.item_create_get);
 /* POST request for creating item. */
 router.post('/item/create', item_controller.item_create_post);
 
-/* GET request to delete item. */
-router.get('/item/:id/delete', item_controller.item_delete_get);
-
+router.get('/item/delete',item_controller.item_delete_get);
 // POST request to delete item
-router.post('/item/:id/delete', item_controller.item_delete_post);
+router.post('/item/delete', item_controller.item_delete_post);
 
 /* GET request to update item. */
-router.get('/item/:id/update', item_controller.item_update_get);
+router.get('/item/update', item_controller.item_update_get);
 
 // POST request to update item
-router.post('/item/:id/update', item_controller.item_update_post);
-
-/* GET request for one item. */
-router.get('/item/:id', item_controller.item_detail);
+router.post('/item/update', item_controller.item_update_post);
 
 /* GET request for list of all items. */
 router.get('/items', item_controller.item_list);
