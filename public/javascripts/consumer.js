a2f5af590911ed2f65b09cea9b5a3b96acd65fae
Reneicko Getten <rkgetten@gmail.com>
2017-11-19 02:50:06 -0500

    added function to combine orders and totaldue

--- public/javascripts/consumer.js
+++ public/javascripts/consumer.js
@@ -1,13 +1,17 @@
 // A $( document ).ready() block.
+/* jshint esversion: 6 */
 $( document ).ready(function() {
+    
+
     var orderlist=[];
     let totaldue=0;
+    var counter=0;
+    var ordercombined="";
     $("tbody > tr").on("mouseover",function(){
         var listorder= $(this).attr('class');
         console.log(listorder + 'consumer');
         $(this).unbind('mouseover');
         $("."+listorder).find("button.add").click(function(){
-
             var name=$("."+listorder).find("td.itemname").text();
           
             console.log(name);
@@ -25,13 +29,36 @@ $( document ).ready(function() {
              tot=Number(tot);
             // console.log(tot);
              totaldue+=tot;
+             let list=$(".orders");
+             let parent=list.parent();
+             list.empty().each(function(){
+              for(let x of orderlist)
+              {
+              
+                  $(this).append('<li>'+x+'</li>');
+                  if(x===orderlist.length-1){
+                      $(this).appendTo(parent);
+                  }
+              }
+              console.log(orderlist);
+             })
+            
            }
            for(let i of orderlist)
            {
-               console.log(i + ' array');
+             ordercombined+=i;
            }
-           console.log("Current total"+ totaldue);
+           ordercombined=ordercombined.split(' ').filter(function(item,i,allItems){
+            return i===allItems.indexOf(item);
+        }).join(' ');
+         
+           console.log("Current total" + totaldue);
+           $(".tot").html(totaldue);
+           console.log(ordercombined + "Combined");
+           $(".ordernames").val(ordercombined);
+           $(".tot").val(totaldue);
         })
+        
     })
   
 });
