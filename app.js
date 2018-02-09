1382849b42254082a7466ee043f5027a2c91da2e
Reneicko Getten <rkgetten@gmail.com>
2018-02-09 14:56:27 -0500

    revert

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var mongo=require('mongodb');
 var mongoose=require('mongoose');
 //mongodb://127.0.0.1/chipsinv -localhost db
 //mongodb://root:bn33bn33@ds225028.mlab.com:25028/chipsinventory
-mongoose.connect('mongodb://root:D4T5b*u%3@ds225028.mlab.com:25028/chipsinventory',{
+mongoose.connect('mongodb://root:bn33bn33@ds225028.mlab.com:25028/chipsinventory',{
   useMongoClient:true
 });
 var db=mongoose.connection;
